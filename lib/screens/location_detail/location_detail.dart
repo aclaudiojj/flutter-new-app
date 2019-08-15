8ceca0bc1087c32fd877a6f9eeda87ded6492e97
“Claudio <aclaudiojj@gmail.com>
2019-08-15 20:06:25 -0300

    models

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:my_other_app/models/location.dart';
 import 'text_section.dart';
 import 'image_banner.dart';
 
 class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<Location> locations = Location.fetchAll();
+    final Location location = locations.first;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Location Detail'),
+        title: Text(location.name),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          ImageBanner('assets/images/kiyomizu-dera.jpg'),
-          TextSection('TITULO 1', 'PRIMEIRA DESCRICAO AQUI'),
-          TextSection('TITULO 2', 'SEGUNDA DESCRICAO AQUI'),
-          TextSection('TITULO 3', 'TERCEIRA DESCRICAO AQUI'),
-        ],
+          ImageBanner(location.imagePath),
+        ]..addAll(this.textSections(location)),
       ),
     );
+  }
+
+  List<Widget> textSections(Location location) {
+    return location.facts
+        .map((fact) => TextSection(fact.title, fact.text))
+        .toList();
   }
 }
